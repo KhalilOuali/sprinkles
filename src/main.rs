bb3c3cfd7112ce96841e45b0c2de91e6ff4cbe4d
Khalil Ouali <khalilouali01@gmail.com>
2023-07-11 19:50:15 +0100

    Refactor and add/change messages and errors
    - replace multiple usage prints with a function
    - add --help message
    - rename read_from_io() to read_from_stdin()
    - read_from_stdin() now collects a vec and joins it

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,49 @@ use std::{
     process::exit,
 };
 
-fn read_from_io() -> String {
+fn print_usage(full: bool) {
+    println!();
+    println!("Usage:");
+    println!("\t- sprinkles");
+    if full {
+        println!("Reads from stdin or pipe. Press Ctrl+D twice to finish typing.");
+    }
+    println!();
+    println!("\t- sprinkles <input>");
+    if full {
+        println!("Reads from argument. Make sure to use quotes if it's multiple words.");
+    }
+    println!();
+    println!("\t- sprinkles -f <filename>");
+    if full {
+        println!("Reads from the file.");
+    }
+    println!();
+    println!("\t- sprinkles --help");
+    if full {
+        println!("Show this help message.");
+    }
+}
+
+fn read_from_stdin() -> String {
     io::stdin()
         .lines()
-        .map(|line| line.expect("Unexpected error: Couldn't read stdin.") + "\n")
-        .collect()
+        .map(|line| line.expect("Unexpected error: Couldn't read stdin."))
+        .collect::<Vec<_>>()
+        .join("\n")
 }
 
 fn read_from_arg(mut args: Args) -> String {
     let arg = args
         .next()
         .expect("Unexpected error: Couldn't parse argument.");
-    if arg == "-f" {
-        eprintln!("Invalid arguments. Usage:");
-        eprintln!("1. sprinkles");
-        eprintln!("2. sprinkles <input>");
-        eprintln!("3. sprinkles -f <filename>");
-        exit(1);
+    if arg == "--help" {
+        println!(
+            "{} prints out a colored version of the input text or file.",
+            sprinklize("sprinkles")
+        );
+        print_usage(true);
+        exit(0);
     };
     arg
 }
@@ -32,10 +58,8 @@ fn read_from_file(mut args: Args) -> String {
         .next()
         .expect("Unexpected error: Couldn't parse arguments.");
     if flag != "-f" {
-        eprintln!("Invalid arguments. Usage:");
-        eprintln!("1. sprinkles");
-        eprintln!("2. sprinkles <input>");
-        eprintln!("3. sprinkles -f <filename>");
+        eprintln!("Invalid arguments.");
+        print_usage(false);
         exit(1);
     };
     let filename = args
@@ -57,14 +81,12 @@ fn read_string() -> String {
     let len = args.len();
 
     match len {
-        0 => read_from_io(),
+        0 => read_from_stdin(),
         1 => read_from_arg(args),
         2 => read_from_file(args),
         _ => {
-            eprintln!("Invalid number of arguments. Usage:");
-            eprintln!("1. sprinkles");
-            eprintln!("2. sprinkles <input>");
-            eprintln!("3. sprinkles -f <filename>");
+            eprintln!("Invalid number of arguments.");
+            print_usage(false);
             exit(1);
         }
     }
@@ -94,13 +116,15 @@ fn sprinklize(string: &str) -> String {
         .map(|c| {
             let color = colors
                 .choose(&mut rng)
-                .expect("Unexpected error: Chosen color null.");
+                .expect("Unexpected error: Chosen color none.");
             c.to_string().color(*color).to_string()
-        }).collect()
+        })
+        .collect()
 }
 
 fn main() {
     let string = read_string();
     let sprinkles = sprinklize(&string);
     println!("{sprinkles}");
+    exit(0);
 }
