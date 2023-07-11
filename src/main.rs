9628d55bd1881df3106b25cf24f07361e16aeee3
Khalil Ouali <khalilouali01@gmail.com>
2023-07-11 01:01:28 +0100

    Add option to sprinklize argument or stdin + some refactoring
    main calls read_string, which depending on number of args, calls
      1. read_from_io (0 args, enables piping in, ctrl+d to stop input)
      2. read_from_arg (1 arg != -f, between "" if there are spaces)
      3. read_from_file (2 args: -f <filename>)
    then colors string and prints it out.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,76 @@
 use colored::{Color, Colorize};
 use rand::seq::SliceRandom;
-use std::{env::args, fs, process::exit};
+use std::{
+    env::{args, Args},
+    fs, io,
+    process::exit,
+};
 
-fn main() {
-    let mut args = args();
-    let exe_name = args.next().unwrap_or("sprinkles".to_string());
+fn read_from_io() -> String {
+    io::stdin()
+        .lines()
+        .map(|line| line.expect("Unexpected error: Couldn't read stdin.") + "\n")
+        .collect()
+}
 
-    if args.len() != 1 {
-        eprintln!("Invalid number of arguments.");
-        eprintln!("Usage: {exe_name} <filename>");
+fn read_from_arg(mut args: Args) -> String {
+    let arg = args
+        .next()
+        .expect("Unexpected error: Couldn't parse argument.");
+    if arg == "-f" {
+        eprintln!("Invalid arguments. Usage:");
+        eprintln!("1. sprinkles");
+        eprintln!("2. sprinkles <input>");
+        eprintln!("3. sprinkles -f <filename>");
         exit(1);
-    }
+    };
+    arg
+}
 
+fn read_from_file(mut args: Args) -> String {
+    let flag = args
+        .next()
+        .expect("Unexpected error: Couldn't parse arguments.");
+    if flag != "-f" {
+        eprintln!("Invalid arguments. Usage:");
+        eprintln!("1. sprinkles");
+        eprintln!("2. sprinkles <input>");
+        eprintln!("3. sprinkles -f <filename>");
+        exit(1);
+    };
     let filename = args
         .next()
         .expect("Unexpected error: Couldn't parse filename.");
-
-    let content = match fs::read_to_string(filename) {
-        Ok(s) => s,
+    match fs::read_to_string(filename) {
+        Ok(content) => content,
         Err(error) => {
             eprintln!("Couldn't read file due to the following error:");
             eprintln!("{error}");
             exit(2);
         }
-    };
+    }
+}
 
+fn read_string() -> String {
+    let mut args = args();
+    args.next();
+    let len = args.len();
+
+    match len {
+        0 => read_from_io(),
+        1 => read_from_arg(args),
+        2 => read_from_file(args),
+        _ => {
+            eprintln!("Invalid number of arguments. Usage:");
+            eprintln!("1. sprinkles");
+            eprintln!("2. sprinkles <input>");
+            eprintln!("3. sprinkles -f <filename>");
+            exit(1);
+        }
+    }
+}
+
+fn sprinklize(string: &str) -> String {
     let colors = [
         Color::Red,
         Color::Green,
@@ -43,13 +89,18 @@ fn main() {
     ];
 
     let mut rng = rand::thread_rng();
-    content
+    string
         .chars()
         .map(|c| {
             let color = colors
                 .choose(&mut rng)
                 .expect("Unexpected error: Chosen color null.");
-            c.to_string().color(*color)
-        })
-        .for_each(|c| print!("{c}"));
+            c.to_string().color(*color).to_string()
+        }).collect()
+}
+
+fn main() {
+    let string = read_string();
+    let sprinkles = sprinklize(&string);
+    println!("{sprinkles}");
 }
